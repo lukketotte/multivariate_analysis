400d14b9edef8841218891d7a45c893c0bcb31a6
Lukas Arnroth <lukas.arnroth@gmail.com>
2017-10-29 20:28:28 +0100

    adding function that creates rules for observed data and confusion matrix

--- disc.py
+++ disc.py
@@ -69,35 +69,83 @@ class disc:
 	# falls into the first population and
 	# FALSE otherwise
 	# if no X0 is supplied, only give crit value
-	def getRule(self, c1, c2, p1, p2, x0 = None):
+	def getRule(self, c1 = 1, c2 = 1, 
+		        p1 = 1, p2 = 1, x0 = None):
 		# ruling will differ depending on value of const
 		const = (c1/c2)*(p2/p1)
 		yhat = self.getLinearDiscF()
 
-		if x0 == None and const == 1:
-			return const
-		elif x0 == None and const != 1:
+		if const == 1 and x0 == None:
+			return (1/2) * ((self.x1_b-self.x2_b).T * LA.inv(self.Sp) * (self.x1_b + self.x2_b))
+		# if the whole cost prior thing is 1 the ruling looks
+		# a little different than for not 1
+		elif const == 1 and x0 != None:
+			m = (self.x1_b-self.x2_b).T * LA.inv(self.Sp) * (self.x1_b + self.x2_b)
+			obs = yhat * x0
+			# get the ruling (TRUE if 1, false otherwise)
+			if obs > m:
+				ret = True
+			elif obs < m:
+				ret = False
+		elif const != 1 and x0 != None:
 			return math.log(const)
 		else:
-		    # if the whole cost prior thing is 1 the ruling looks
-		    # a little different than for not 1
-			if const == 1:
-				m = (self.x1_b-self.x2_b).T * LA.inv(self.Sp) * (self.x1_b + self.x2_b)
-				obs = yhat * x0
-				# get the ruling (TRUE if 1, false otherwise)
-				if obs > m:
-					ret = True
-				elif obs < m:
-					ret = False
+			m = math.log(const)
+			obs = yhat - 1/2 * yhat * (self.x1_b + self.x2_b)
+			if obs > m:
+				ret = True
 			else:
-				m = math.log(const)
-				obs = yhat - 1/2 * yhat * (self.x1_b + self.x2_b)
-				if obs > m:
-					ret = True
-				else:
-					ret = False
+				ret = False
+		return ret
 
-			return ret
+	# uses the getRule function, taking the same 
+	# parameters as that function. 
+	# sampleGrp is the group for which to get
+	# the confusion matrix, defaults to pop 1
+	def getConfMat(self, c1 = 1, c2 = 1, 
+		           p1 = 1, p2 = 1):
+		# we need a'x0
+		# where x0 will be the data matrix
+		ldf = self.getLinearDiscF()
+		# problem: can't follow the 11-19 formula
+		# 		   have to take transpose of the x
+		obs1 = ldf * self.X1.T			
+		obs2 = ldf * self.X2.T
+		# vectorize the obs matricies
+		obs1 = obs1.getA1()
+		obs2 = obs2.getA1()
+		# get ruling
+		m = self.getRule(c1, c2, p1, p2)
+
+		# initiate return objects
+		grp1 = np.empty([self.X1.shape[0], 2])
+		grp2 = np.empty([self.X2.shape[0], 2])
+		# the confusion matrix will be updated
+		# continously
+		confMat = np.zeros([2,2])
+
+		# go through X1 and make rulings
+		for i in range(self.X1.shape[0]):
+			grp1[i, 0] = obs1[i]
+			if obs1[i] > m:
+				grp1[i, 1] = 1 # correctly classified
+				confMat[0, 0] += 1
+			else:
+				grp2[i, 1] = 2 # incorrectly classified
+				confMat[0, 1] += 1
+
+		# go through X2 and make rulings
+		for i in range(self.X2.shape[0]):
+			grp2[i, 0] = obs2[i]
+			if obs2[i] > m:
+				grp2[i, 1] = 1 # incorrectly classified
+				confMat[1,0] += 1
+			else:
+				grp2[i, 1] = 2 # correctly classified
+				confMat[1,1] += 1
+
+		return grp1, grp2, confMat
+
 
 	# access the model matricies
 	# allSigma = False gives only pooled
